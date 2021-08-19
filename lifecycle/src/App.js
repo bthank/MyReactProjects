41de619e1f640829785da8afe29ab4890db296b0
bthank <bthankachan@yahoo.com>
2021-08-19 12:24:04 -0400

    Added lifecycle app

---
+++ lifecycle/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from "react";  
+
+class App extends Component {
+  /* set initial state of component in constructor */
+  constructor(props){
+    super(props);
+    console.log('In constructor of App component');
+    
+    this.state = {count: 0};
+  }
+
+  handleClick() {
+    this.setState({count: this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClick.bind(this)}>Click Me</button>
+        <br/>
+        {this.state.count}
+      </div>
+    );
+  }
+
+
+  
+}
+
+export default App;
