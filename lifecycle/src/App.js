8af6c116e119a15cdc9d59752e2539d49e5df282
bthank <bthankachan@yahoo.com>
2021-08-19 13:47:37 -0400

    Added useEffect React hookto lifecycle app

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";  /* useState is a React hook */
+import React, { useEffect, useState } from "react";  /* useState is a React hook */
 
 function App() {
   
     /* initializes count to 0 and setCount to the useState(0) function */
     const [count,setCount] = useState(0);
-     
-
+    const [data, setData] = useState([]);
+    
+    /* add a callback function to useEffect React hook*/
+    /* useEffect hook gets invoked immediately after every time the render() happens */ 
+    useEffect(()=>{
+      if (count%3 === 0) {
+        document.title = count;
+      }
+    })
    
     return (
       <div className="App">
-        <button onClick={this.handleClick.bind(this)}>Click Me</button>
-        <br/>
-        {this.state.count}
+        <button onClick= {() => setCount(count+1)} >Click Me</button>
+         
       </div>
     );
    
